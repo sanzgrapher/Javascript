cf90e1c1940b2e87c2726aa8a8e5dbec56d1519e
Narayan Dhakal <narayandhakal443@gmail.com>
2022-06-06 21:20:36 +0545

    Hoisting in js

--- Basics of js/variable_hoisting.js
+++ Basics of js/variable_hoisting.js
@@ -1,6 +1,8 @@
 // move the declaration up and down to get clear view of hoisting
 // gives undefined when it is below the use and only iniciate but not declare
 
+//////////////////////////////////////////////
+
 var str = "subash";
 console.log(str);
 
@@ -8,3 +10,31 @@ console.log(str);
 
 console.log(a);
 var a = 5; 
+
+//////////////////////////////////////////////
+
+var x;
+x = 3;
+console.log(x); // prints 3
+
+var y;
+console.log(y); // undefined
+y = 2;
+
+z = 4;
+console.log(z); // prints 4
+var y;
+
+console.log(c); // undefined
+var c = 5;  //  JavaScript only hoists declarations, not initializations.
+
+//////////////////////////////////////////////
+
+
+var myvar = 'my value';
+(function () {
+    var myvar;
+    console.log(myvar); // undefined
+    myvar = 'local value';
+})();
+//////////////////////////////////////////////
