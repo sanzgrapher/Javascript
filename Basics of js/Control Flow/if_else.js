c3616fc3e0632ed6f39c6f813930617db6418a40
Narayan Dhakal <narayandhakal443@gmail.com>
2022-06-06 23:43:15 +0545

    normal if else to check odd or even

---
+++ Basics of js/Control Flow/if_else.js
@@ -0,0 +1,9 @@
+// check no is odd or even 
+let number = 10;
+const num = prompt("Enter a number check odd or even: ");
+if (number % 2 == 0) {
+    console.log("The number is even.");
+}
+else {
+    console.log("The number is odd.");
+}
