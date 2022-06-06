47373e5619f012b884050b5bf873dc773e8d0356
Narayan Dhakal <narayandhakal443@gmail.com>
2022-06-06 21:13:03 +0545

    Hoisting in js

---
+++ Basics of js/variable_hoisting.js
@@ -0,0 +1,10 @@
+// move the declaration up and down to get clear view of hoisting
+// gives undefined when it is below the use and only iniciate but not declare
+
+var str = "subash";
+console.log(str);
+
+
+
+console.log(a);
+var a = 5; 
