7ba3a09db46ba6655b34321821cfaf52dc2d1161
Narayan Dhakal <narayandhakal443@gmail.com>
2022-06-07 19:31:56 +0545

    charCodeAt to return unicode

---
+++ Basics of js/Strings/charCodeAt.js
@@ -0,0 +1,9 @@
+document.getElementById("title").innerHTML = `charCodeAt(); :: string.charCodeAt(index)`;
+
+const name = 'naran';
+// unicode of the desire character 
+index = 4;
+console.log(`The charcode of ${name.charAt(index)} is ${name.charCodeAt(index)}`);
+// unicode of the last character 
+console.log(name.charCodeAt(name.length-1))
+
