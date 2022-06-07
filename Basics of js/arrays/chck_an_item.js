0ba906a665d3cf6187cafda841d9351b12142a5b
Narayan Dhakal <narayandhakal443@gmail.com>
2022-06-07 07:42:16 +0545

    arrays

---
+++ Basics of js/arrays/chck_an_item.js
@@ -0,0 +1,10 @@
+const array = ['one', 'two', 'three', 'four'];
+
+const hasValue = array.includes('five');
+
+// check the condition
+if (hasValue) {
+    console.log('Array contains a value.');
+} else {
+    console.log('Array does not contain a value.');
+}
